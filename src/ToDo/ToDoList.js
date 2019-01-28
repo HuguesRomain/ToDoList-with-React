eda7e14537755442c6775012df9cd09d41ec2267
HuguesRomain <huguesromainpro@gmail.com>
2019-01-28 13:58:48 +0100

    functional inputs

--- src/ToDo/ToDoList.js
+++ src/ToDo/ToDoList.js
@@ -1,12 +1,31 @@
 import React,  {Component}  from 'react'; 
 
 class ToDoList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      userImputs: '',
+      items: [],
+    }; 
+  }
+  onChange(event){
+    this.setState({
+      userImputs: event.target.value,
+    }, () => console.log(this.state.userImputs));
+  }
+
   render() {
     return(
       <div>
         <h1>My To Do List</h1>
         <form>
-          <input type='text' placeholder="Write somethings"/>
+          <input 
+          value={this.state.userImputs} 
+          type='text' 
+          placeholder="Write somethings"
+          onChange={this.onChange.bind(this)}
+          />
+       
           <button>Add</button> 
         </form>
       </div>
