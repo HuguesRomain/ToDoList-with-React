35c0312519acdc02ae40c1896f6ad1f19bbe09e4
HuguesRomain <huguesromainpro@gmail.com>
2019-01-29 11:47:24 +0100

    adding style and functional deleting elements

--- src/ToDo/ToDoList.js
+++ src/ToDo/ToDoList.js
@@ -14,37 +14,52 @@ class ToDoList extends Component {
     });
   }
 
-  renderToDos(){
-    return this.state.items.map((item) => {
-      return(
-      <div key={item}>
-         {item} | <button>X</button>
-      </div>
-      );
-    });
-  }
-
   addToDo(event) { 
     event.preventDefault(); 
     this.setState({
       userImputs: '',
       items: [...this.state.items, this.state.userImputs]
-    }, () => console.log(this.state));
+    });
   };
+
+  deleteToDo(event) {
+    event.preventDefault(); 
+    var array = this.state.items;
+    var index = array.indexOf(event.target.value);
+    array.splice(index);
+    this.setState({
+      items: array,
+    }); 
+  }
+
+  renderToDos(){
+    return this.state.items.map((item) => {
+      return(
+      <div key={item}>
+         {item} | <button 
+         onClick={this.deleteToDo.bind(this)}
+         className="btn btn-danger">X</button> 
+      </div>
+      );
+    });
+  }
 
   render() {
     return(
       <div>
-        <h1>My To Do List</h1>
-        <form>
+        <h1 align="center">My To Do List</h1>
+        <form className='form-row align-items-center'>
           <input 
             value={this.state.userImputs} 
            type='text' 
            placeholder="Write somethings"
            onChange={this.onChange.bind(this)}
+           className="form-control mb-2"
            />
        
-          <button onClick={this.addToDo.bind(this)}>Add</button> 
+          <button 
+          onClick={this.addToDo.bind(this)}
+          className="btn btn-primary">Add</button> 
         </form>
         <div>
           {this.renderToDos()}
